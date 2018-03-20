f0ec8c2caee7da5810515333c2470f95b67f1726
Enrique Avila <senquevila@gmail.com>
2018-03-19 22:25:54 -0600

    Polimorfismo

---
+++ codigo/207-Polimorfismo/207_2_uso_sobrecarga.py
@@ -0,0 +1,11 @@
+import vector
+
+def usar_suma(a, b):
+    print(a + b)
+
+
+usar_suma(1, 2)
+usar_suma(3.0, 1.5)
+usar_suma(True, False)
+usar_suma('hola', 'mundo')
+usar_suma(vector.Vector(1, 1), vector.Vector(4, 5))
---
+++ codigo/207-Polimorfismo/vector.py
@@ -0,0 +1,30 @@
+class Vector:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __str__(self):
+        return "({}, {})".format(self.x, self.y)
+        
+    def __add__(self, other):
+        sumax = self.x + other.x
+        sumay = self.y + other.y
+        vector_suma = Vector(sumax, sumay)
+        return vector_suma
+
+    def __sub__(self, other):
+        restax = self.x - other.x
+        restay = self.y - other.y
+        vector_resta = Vector(restax, restay)
+        return  vector_resta
+
+def main():
+    v1 = Vector(1, 0)
+    v2 = Vector(0, 1)
+    v3 = v1 + v2
+    v4 = v3 - v2
+    print('{} + {} = {}'.format(v1, v2, v3))
+    print('{} - {} = {}'.format(v3, v2, v4))
+
+if __name__ == '__main__':
+    main()
